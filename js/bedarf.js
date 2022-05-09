0ff56c814622f7bcd25818ae86b0b4e4be9af5db
Sett17 <raik.rohde@gmail.com>
2022-05-09 12:21:41 +0200

    vacation: view and js to build it
    fixes #12

---
+++ js/bedarf.js
@@ -0,0 +1,12 @@
+
+function makeVMCard(vm) {
+
+}
+
+function viewVMCards(params) {
+  // backend aufrufen
+  // daten parsen
+  // makeVMCard karten erstellen
+  // content ausleeren
+  // appendChild machen auf content
+}
--- js/main.js
+++ js/main.js
@@ -1,47 +1,9 @@
 //check if not logged in -> login page
 
-let content = document.getElementById('content');
-
-// das is gerad nur zum testen
-for (const i of Array(70).keys()) {
-  content.appendChild(
-    makeVacationCard({start: `2019-01-${i}`, end: `2019-01-${i+1}`})
-  );
-}
-
-function makeGenericCard(title, fragment) {
+function makeGenericCard(fragment) {
   let clone = document.getElementById('card-template').content.cloneNode(true)
-  clone.querySelector('h3').textContent = title;
   clone.querySelector('.card').appendChild(fragment)
   return clone;
 }
 
-function makeVacationCard(vacation) {
-  let clone = document.getElementById('card-info-template').content.cloneNode(true)
-  let cloneInfo = clone.querySelector('.card-info');
-  cloneInfo.appendChild(document.createElement('span')).textContent = `Start: ${vacation.start}`
-  cloneInfo.appendChild(document.createElement('span')).textContent = `End: ${vacation.end}`
-  let card = makeGenericCard('Vacation', clone)
-  return card
-}
 
-
-function viewVacationCards(params) {
-  // backend aufrufen
-  // daten parsen
-  // makeVacationCard karten erstellen
-  // content ausleeren
-  // appendChild machen auf content
-}
-
-function makeVMCard(vm) {
-
-}
-
-function viewVMCards(params) {
-  // backend aufrufen
-  // daten parsen
-  // makeVMCard karten erstellen
-  // content ausleeren
-  // appendChild machen auf content
-}
