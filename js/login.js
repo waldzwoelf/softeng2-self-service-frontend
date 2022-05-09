9c7a4caa15be9056a5d29f65f3442ad63fb15d7e
Sett17 <raik.rohde@gmail.com>
2022-05-09 10:42:04 +0200

    remove old js login code

--- js/login.js
+++ js/login.js
@@ -1,11 +1,2 @@
 // logic fuer login backend requests
 // check if already logged in? (kommt aufs backend an ob das so geht)
-
-function myFunction() {
-    var x = document.getElementById("myPwd");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  } 
