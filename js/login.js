934402bc0e82b7577471aa0b0e9bd14ddfe31882
Jan Baumann <jan.bmnn@gmail.com>
2022-05-14 13:03:12 +0200

    Minor tweaks

--- js/login.js
+++ js/login.js
@@ -1,2 +1,17 @@
 // logic fuer login backend requests
 // check if already logged in? (kommt aufs backend an ob das so geht)
+
+
+
+/*
+
+const hasAccount = document.querySelector('#switch').value //...
+
+hasAccount ? // change title, button text and visibility of role select field
+
+:  // leave as is
+
+
+
+*/
+
