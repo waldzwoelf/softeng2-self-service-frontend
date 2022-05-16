2eb9aebb55f0794cfd91ea38d7797fed61aa164a
Jan Baumann <jan.bmnn@gmail.com>
2022-05-16 10:38:39 +0200

    Added Toggle Button to toggle between login and registration

--- js/login.js
+++ js/login.js
@@ -2,16 +2,28 @@
 // check if already logged in? (kommt aufs backend an ob das so geht)
 
 
+function toggle() {
+    let register = document.getElementById('register-header');
+    let login = document.getElementById('login-header');
+    let roleSelect = document.getElementById('role');
+    let roleLabel = document.getElementById('role-label');
+    let switchValue = document.getElementById('switch-value').checked;
+    let buttonText = document.getElementById('submit-button');
+    console.log(switchValue);
+    if (switchValue){
+        register.style.display= 'none';
+        login.style.display = 'block';
+        roleSelect.style.display = 'none';
+        roleLabel.style.display = 'none';
+        buttonText.innerText = 'Sign in'
+    } else {
+        register.style.display = 'block';
+        login.style.display = 'none';
+        roleSelect.style.display = 'block';
+        roleLabel.style.display = 'block';
+        buttonText.innerText = 'Sign up';
+    }
+}
 
-/*
 
-const hasAccount = document.querySelector('#switch').value //...
-
-hasAccount ? // change title, button text and visibility of role select field
-
-:  // leave as is
-
-
-
-*/
 
