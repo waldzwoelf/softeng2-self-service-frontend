ed4231aa469d0ebbdaac0054986b4a1203888aec
Sett <64754924+Sett17@users.noreply.github.com>
2022-06-01 09:43:14 +0200

    added bedarf view with building js, and new btn (#29)
    * added bedarf view with building js, and new btn
    
    * include dbg call in display for now, so we need less changes later
    
    * Userstory: Neuen Urlaubsantrag stellen
    Fixes #21
    
    * Add min value to number_urlaubstage
    
    * Delete logs
    
    * correct information field in bedarf
    
    git history bisschen verdreckt, zu viel arbeit wieder cool zu machen...

--- js/bedarf.js
+++ js/bedarf.js
@@ -1,12 +1,69 @@
+const VMView = {
+  makeCard(vm) {
+    // was fuer infos haben wir hier? die felder hier sind jz ausgedacht
+    let frag = new DocumentFragment()
+    let info = frag.appendChild(document.createElement('div'))
+    info.appendChild(document.createElement('h3')).textContent = `${vm.ipAdress}`
+    info.appendChild(document.createElement('span')).textContent = `Typ: ${vm.virtualMachineType}`
+    info.appendChild(document.createElement('span')).textContent = `Username/Password:`
+    info.appendChild(document.createElement('span')).textContent = `${vm.username}`
+    info.appendChild(document.createElement('span')).textContent = `${vm.initialPassword}`
 
-function makeVMCard(vm) {
+    let status = frag.appendChild(document.createElement('div'))
+    let statusText = status.appendChild(document.createElement('h3'))
+    if (vm.status === 'genehmigt') {
+      //string kommt vom backend dann
+      statusText.textContent = 'Genehmigt'
+      statusText.style.color = 'green'
+    } else if (vm.status === 'abgelehnt') {
+      statusText.textContent = 'Abgelehnt'
+      statusText.style.color = 'red'
+    } else {
+      statusText.textContent = 'Offen'
+      statusText.style.color = 'orange'
+    }
+    frag.appendChild(document.createElement('p')).textContent = vm.description
 
-}
+    let card = makeGenericCard(frag)
+    return card
+  },
+  updateTitle() {
+    document.querySelector('#content > h1').textContent = 'Bedarf'
+  },
+  hideAvailableDays() {
+    document.getElementById('available').style.display = 'none'
+  },
+  updateNewBtn() {
+    document.querySelector('#newBtn a').href = '/new_vm'
+  },
+  display(params) {
+    // die ganzen andere funktionen hier aufrufen, wie im dbg
+    //backend und dann in '#requests' rein
+    // sortien nach status? genehmigt -> offen -> abgelehnt
 
-function viewVMCards(params) {
-  // backend aufrufen
-  // daten parsen
-  // makeVMCard karten erstellen
-  // content ausleeren
-  // appendChild machen auf content
+    //remove this
+    this.dbg()
+  },
+  dbg() {
+    document.querySelectorAll('#requests .card').forEach((it) => {
+      it.remove()
+    })
+    let states = ['genehmigt', 'abgelehnt', 'offen']
+
+    VMView.updateTitle()
+    VMView.hideAvailableDays()
+    VMView.updateNewBtn()
+    for (const i of Array(70).keys()) {
+      document.getElementById('requests').appendChild(
+        VMView.makeCard({
+          virtualMachineType: 0,
+          description: "Dikka was",
+          ipAdress: "255.255.255.255",
+          username: "bvyuk4fds",
+          initialPassword: "fsdfe88943nsifgdf",
+          status: states[i % 3]
+        })
+      )
+    }
+  },
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-//check if not logged in -> login page
+//check if not logged in -> login pag
 
-function makeGenericCard(fragment) {
-  let clone = document.getElementById('card-template').content.cloneNode(true)
-  clone.querySelector('.card').appendChild(fragment)
-  return clone;
-}
-
-
+VacationView.display()
