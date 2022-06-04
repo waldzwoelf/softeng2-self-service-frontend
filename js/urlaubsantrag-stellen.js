af9d4e44777931e9afde071ee44baae93e18e517
Sett17 <raik.rohde@gmail.com>
2022-06-04 09:29:25 +0200

    adjust antrag form and send request to backend, with DBGID fixes #32

--- js/urlaubsantrag-stellen.js
+++ js/urlaubsantrag-stellen.js
@@ -1,19 +1,31 @@
-function toggleGleitzeit() {
+function sendVacationRequest() {
+  // dann gegen richtige ID austauschen, die wa durch den login bkommen
+  let DBGID = -1
 
-    let checkboxGleitzeit = document.getElementById('checkbox_gleitzeit');
-    let isGleitzeitChecked = checkboxGleitzeit.checked;
-    let inputUrlaubstage = document.getElementById('number_urlaubstage'); 
+  let form = document.querySelector('.form-urlaub')
 
+  let data = {
+    userId: DBGID,
+    startDate: new Date(form.querySelector('input[name="von"]').value).getTime(),
+    endDate: new Date(form.querySelector('input[name="bis"]').value).getTime(),
+    comment: form.querySelector('textarea[name="comment"]').value,
+    status: 'beantragt',
+  }
 
-    if (isGleitzeitChecked) {
-        // Checkbox is set to Gleitzeit
-        inputUrlaubstage.readOnly = false;
-        inputUrlaubstage.setAttribute('style', "width: 3em");
+  fetch('https://studium.webfajo.de/mitarbeiter/antragstellung', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((res) => {
+    if (res.status === 200) {
+      alert('Antrag erfolgreich gesendet')
+      window.location.href = '/'
     } else {
-        // Checkbox is set to no Gleitzeit
-        inputUrlaubstage.readOnly = true;
-        inputUrlaubstage.setAttribute('style', "width: 3em; background-color: #fff0e2; border-color: transparent; color: grey;");
-        // TODO set calculated Urlaubstage from VON and BIS date
-            inputUrlaubstage.value = 0;
+      alert('Fehler beim Senden des Antrags')
     }
+  })
+
+  return false
 }
