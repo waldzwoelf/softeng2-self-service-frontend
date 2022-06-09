c88f87a21f0a9e8cec3b628ef39aaa5fb1af51d5
Sett <64754924+Sett17@users.noreply.github.com>
2022-06-09 11:11:25 +0200

    Revert "Changed implemented API POST call for VM request"
    This reverts commit 4a8384fce9a539062665978d959fd783ad931b09.

--- js/bedarf-stellen.js
+++ js/bedarf-stellen.js
@@ -7,12 +7,11 @@ function submitProvisioningRequest(event) {
 
 
     let data = {
-        virtualMachineType: parseInt(form.querySelector('#environment').value),
+        virtualMachineType: form.querySelector('#environment').value,
         description: form.querySelector('#description').value,
         employeeEmailAddress: form.querySelector('#email').value
 
     }
-    console.log(data);
 
     let request = new Request(provAPI, {
         method: 'POST',
@@ -24,9 +23,9 @@ function submitProvisioningRequest(event) {
 
     fetch(request)
         .then((res) => {
-            if (res.status === 201) {
+            if (res.status === 200) {
                 alert('Antrag erfolgreich gesendet!')
-                // window.location.href = '/'
+                window.location.href = '/'
             } else {
                 alert('Fehler beim Senden des Requests')
             }
