ceb9ffe24e277d6364425eb716d8c5de31fb4c04
jbaumann <jan.bmnn@gmail.com>
2022-06-14 11:05:12 +0200

    stuff
    Removed comments and double function call, included login.js in new_vm/index.html, now using email address of logged in user instead of manually adding it to request through form, now fetching data with Auth token, created another makeinfocard for different html segment (would always display in 'request')

--- js/bedarf-stellen.js
+++ js/bedarf-stellen.js
@@ -1,25 +1,33 @@
 let form = document.querySelector('#form-vm');
 
-function submitProvisioningRequest(event) {
-    event.preventDefault();
+async function submitProvisioningRequest(event) {
+     event.preventDefault();
+
+
+    let user = await getUser()
+
 
 
     let data = {
         virtualMachineType: parseInt(form.querySelector('#environment').value),
         description: form.querySelector('#description').value,
-        employeeEmailAddress: form.querySelector('#email').value
+        employeeEmailAddress: user.email
 
     }
+
 
     fetch('https://provisioningserviceapi.azurewebsites.net/provisioning/api/RequisitionNotes', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getCookie('JWT')
         },
         body: JSON.stringify(data),
     }).then((res) => {
         if (res.status === 201) {
             alert('Antrag abgeschickt!')
+            window.location.href = '/'
+            // VMView.display()
         } else {
             alert('Etwas ist schief gelaufen...')
         }
--- js/misc.js
+++ js/misc.js
@@ -10,3 +10,11 @@ function makeInfoCard(info) {
   div.appendChild(document.createElement('h3')).textContent = `${info}`
   document.getElementById('requests').appendChild(makeGenericCard(frag))
 }
+
+
+function makeInfoCard2(info){
+ let frag = document.createDocumentFragment()
+  let div = frag.appendChild(document.createElement('div'))
+  div.appendChild(document.createElement('h3')).textContent = `${info}`
+  document.getElementById('available-vms').appendChild(makeGenericCard(frag))
+}
