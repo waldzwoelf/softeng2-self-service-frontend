24e04ecc99728742cbc288a89236022ca26284e2
jbaumann <jan.bmnn@gmail.com>
2022-06-14 13:55:11 +0200

    In  ApprovalView make Cards with 2 buttons for approval and rejection
    getting data from parent elements to build data (might be shitty) Posting to VirtualEnvironments and Putting to RequisitionNotes on Button click

--- js/ApprovalView.js
+++ js/ApprovalView.js
@@ -1,11 +1,25 @@
 const ApprovalView = {
 
 
-    makeCard(type='available', vm) {
+    makeCard(type, vm) {
+        console.log(vm);
+        let vmTypes = {
+            0: "WindowsServer2022",
+            1: "Windows11",
+            2: "Ubuntu2204"
+        }
 
-        if (type === 'request') {
+        let vmTypesReverse = {
+            "WindowsServer2022": 0,
+            "Windows11": 1,
+            "Ubuntu2204": 2
+        }
+
+        if (type === 'approval'){
+
             let frag = new DocumentFragment()
             let info = frag.appendChild(document.createElement('div'))
+            info.appendChild(document.createElement('h2')).textContent = `${vm.id}`
             info.appendChild(document.createElement('h3')).textContent = `${vm.employeeEmailAddress}`
             info.appendChild(document.createElement('span')).textContent = `Typ: ${vmTypes[vm.virtualMachineType]}`
             info.appendChild(document.createElement('p')).textContent = vm.description
@@ -22,10 +36,79 @@ const ApprovalView = {
                 statusText.style.color = 'orange'
             }
 
+
+            let buttonApprove = document.createElement('button')
+            buttonApprove.classList.add('btn')
+            buttonApprove.id = 'buttonApprove'
+            buttonApprove.textContent = 'Accept'
+            info.appendChild(buttonApprove)
+            buttonApprove.addEventListener('click',function () {
+                console.log('Approve');
+                let vmTypeText = this.parentElement.querySelector('span').textContent
+                let vmTypeTextText = vmTypeText.substring(vmTypeText.search(' ') + 1 ,vmTypeText.length)
+                let converted = vmTypesReverse[vmTypeTextText]
+                let id = this.parentElement.querySelector('h2').textContent
+                let vmToApprove = {
+                    virtualMachineType: converted,
+                    description: this.parentElement.querySelector('p').textContent,
+                    employeeEmailAddress: this.parentElement.querySelector('h3').textContent,
+                    ipAddress: '127.0.0.1',
+                    username: 'initialUsername',
+                    initialPassword: 'initialPassword'
+                }
+                let requestToPutApproved = {
+                    reasonForRejection: "",
+                    rejected: false
+                }
+                console.log(vmToApprove);
+                console.log(requestToPutApproved);
+                fetch('https://provisioningserviceapi.azurewebsites.net/provisioning/api/VirtualEnvironments',{
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + getCookie('JWT')
+                    },
+                    body: JSON.stringify(vmToApprove)
+                }).then((res) =>{
+                    if (res.status === 200){
+                        alert('Succes!')
+                    }else if (res.status === 400){
+                        alert('nope.')
+                    }
+
+                })
+                fetch(`https://provisioningserviceapi.azurewebsites.net/provisioning/api/RequisitionNotes/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + getCookie('JWT')
+                    },
+                    body: JSON.stringify(requestToPutApproved)
+
+                }).then((res) => {
+                    if (res.status === 204 || res.status === 200){
+                        console.log('Yay');
+                    }else {
+                        console.log('nopppioo');
+                    }
+                })
+            })
+
+            let buttonReject = document.createElement('button')
+            buttonReject.classList.add('btn')
+            buttonReject.id = 'buttonReject'
+            buttonReject.textContent = 'Reject'
+            buttonReject.addEventListener('click', function () {
+                console.log('Reject');
+            })
+            info.appendChild(buttonReject)
+
+
+
             let card = makeGenericCard(frag)
             return card
-
         }
+
 
 
     },
